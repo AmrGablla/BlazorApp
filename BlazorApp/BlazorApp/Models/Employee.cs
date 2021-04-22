a3d5ea12a93fcb915a86f7ffc53333c982ea6fa4
amr <amrjablla@gmail.com>
2021-04-22 14:48:55 +0200

    init

---
+++ BlazorApp/BlazorApp/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Models
+{
+    public class Employee
+    {
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ BlazorApp/BlazorApp/Services/EmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using BlazorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Services
+{
+    public class EmployeeService
+    {
+        private static List<Employee> employees = new List<Employee>();
+        public EmployeeService()
+        { 
+        }
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return employees.AsEnumerable();
+        }
+        public void Add(Employee employee)
+        {
+            employees.Add(employee);
+        }
+    }
+}
